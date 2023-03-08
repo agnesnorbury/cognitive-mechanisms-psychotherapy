faa3cf7771a579800526ba21fd97fe0e557f3969
Agnes Norbury <anorbury@imac-mpc.local>
2023-03-08 15:27:21 +0000

    renaming

--- reward-effort-planning/public/js/eventsCenter.js
+++
@@ -1,5 +0,0 @@
-// create our own events emitter for passing information about events between scenes (without conflicting with this.game.events which is not recommended for this purpose)
-
-const eventsCenter = new Phaser.Events.EventEmitter()
-
-export default eventsCenter
